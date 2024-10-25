a3804592da548dcc33eca151c26d9a0f93ced8c4
Jamanul <jk.skb.25@gmail.com>
2024-10-25 19:28:26 +0600

    added all the packacges and installed react

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,9 @@
-<div className="card bg-base-100 w-96 shadow-xl">
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card bg-base-100 w-96 shadow-xl">
   <figure>
     <img
       src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
@@ -12,3 +17,9 @@
     </div>
   </div>
 </div>
+
+    </div>
+  );
+};
+
+export default Card;
